f5b40cdb59dbbb2dae6157993bf9f76a4b817adb
Kimmo Riipinen <riipinenkimmo@gmail.com>
2024-08-28 14:52:07 +0300

    change scene update

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class ChangeScenes : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,10 @@ public class SceneManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("ManagementMenuScene");
     }
 }
